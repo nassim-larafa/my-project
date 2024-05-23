d4a8d155f435ddcf906751e48a1a38214b6737ac
Nassim Larafa <75386569+nassim-larafa@users.noreply.github.com>
2024-05-23 22:00:27 +0100

    styling-2

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+// Login.js
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity, Animated, Image } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const logoOpacity = useRef(new Animated.Value(0)).current; // Initial opacity value of 0
+
+  useEffect(() => {
+    Animated.timing(logoOpacity, {
+      toValue: 1, // Final opacity value
+      duration: 2000, // Duration of the animation in milliseconds
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
   const handleLogin = async () => {
     try {
@@ -23,7 +34,10 @@ const Login = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      
+      <Animated.Image
+        source={require('../assets/images/logo.png')}
+        style={[styles.logo, { opacity: logoOpacity }]}
+      />
       <Text style={styles.title}>Login</Text>
       <TextInput
         style={styles.input}
@@ -51,11 +65,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
-    backgroundColor: '#7FC3CA', // background color
+    backgroundColor: '#7FC3CA', // F background color
+  },
+  logo: {
+    position: 'absolute',
+    top: 30, // Adjust as needed
+    right: 155, // Adjust as needed
+    width: 60,
+    height: 60,
+    borderRadius:50,
   },
   title: {
-    fontSize: 26,
+    fontSize: 27,
     marginBottom: 16,
     textAlign: 'center',
     color:'white',
@@ -66,9 +89,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 12,
     paddingHorizontal: 8,
+    width: '80%',
   },
   button: {
-    backgroundColor: '#A3EB6A', //  background color
+    backgroundColor: '#A3EB6A', // Dodger blue background color
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
